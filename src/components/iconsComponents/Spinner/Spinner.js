c59a0cd2e0429297d57d3a679d387585410c6551
Kraisser <vgaiko@mail.ru>
2022-11-30 13:55:56 +0300

    6.6.5 small fix userIcon

--- src/components/iconsComponents/Spinner/Spinner.js
+++ src/components/iconsComponents/Spinner/Spinner.js
@@ -1,9 +1,9 @@
 import './spinner.css';
 
-export default function Spinner() {
+export default function Spinner({width, height, padding}) {
 	return (
-		<div className='spinnerWrapper'>
-			<div className='cssload-loader'>
+		<div className='spinnerWrapper' style={{padding}}>
+			<div className='cssload-loader' style={{width, height, padding}}>
 				<div className='cssload-inner cssload-one'></div>
 				<div className='cssload-inner cssload-two'></div>
 				<div className='cssload-inner cssload-three'></div>
