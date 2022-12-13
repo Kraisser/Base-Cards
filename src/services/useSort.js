79bdd79f0c781fb9c5d30619a64670c6bd2ecd3f
Kraisser <vgaiko@mail.ru>
2022-12-13 22:54:19 +0300

    6.6.6 fixed mozila sort bug

--- src/services/useSort.js
+++ src/services/useSort.js
@@ -1,11 +1,15 @@
 export default function useSort() {
 	const sortChaptersByName = (chapArr) => {
 		if (chapArr) {
-			return [...chapArr].sort((prevItem, item) =>
-				prevItem.id === 'favorite+chapter'
-					? -1
-					: prevItem.name.localeCompare(item.name, 'ru', {ignorePunctuation: true})
-			);
+			return [...chapArr].sort((prevItem, item) => {
+				if (prevItem.id === 'favorite+chapter') {
+					return -1;
+				}
+				if (item.id === 'favorite+chapter') {
+					return 1;
+				}
+				return prevItem.name.localeCompare(item.name, 'ru', {ignorePunctuation: true});
+			});
 		}
 		return [];
 	};
